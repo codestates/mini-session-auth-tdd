d8d9275611e0bf188c8409f875366b12300e673b
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2019-12-11 22:16:34 +0900

    Initial server test

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * api/app.js
+ * exports an express app.
+ */
+
+const express = require("express");
+//initializes express app
+const app = express();
+
+module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
-var assert = require("assert");
-describe("Array", function() {
-  describe("#indexOf()", function() {
-    it("should return -1 when the value is not present", function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+/**
+ * test/test.js
+ * Basic tests for Auth system API
+ */
+const chai = require("chai");
+const expect = chai.expect;
+
+//start app
+const app = require("../app");
+
+describe("App", () => {
+  it("Should exists", () => {
+    expect(app).to.be.a("function");
   });
 });
