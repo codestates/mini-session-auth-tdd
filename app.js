a7bab03f6deeecca016490e49cbe610e21413b4f
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2019-12-11 22:20:23 +0900

    Test basic get request

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 /**
  * api/app.js
- * exports an express app.
+ * exports an express app started.
  */
 
 const express = require("express");
-//initializes express app
 const app = express();
+
+const bodyParser = require("body-parser");
+
+//middleware to parse requests of extended urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+//middleware to parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+//endpoints
+app.get("/", (req, res) => {
+  res.send({ message: "Yabadabadooo" });
+});
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -2,14 +2,34 @@
  * test/test.js
  * Basic tests for Auth system API
  */
+
 const chai = require("chai");
 const expect = chai.expect;
+//import chai-http to send requests to the app
+const http = require("chai-http");
+chai.use(http);
 
-//start app
 const app = require("../app");
 
-describe("App", () => {
+describe("App basics", () => {
   it("Should exists", () => {
     expect(app).to.be.a("function");
+  });
+
+  it("GET / should return 200 and message", done => {
+    //send request to the app
+    chai
+      .request(app)
+      .get("/")
+      .then(res => {
+        //assertions
+        //console.log(res.body);
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.contain("Yabadabadooo");
+        done();
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
   });
 });
